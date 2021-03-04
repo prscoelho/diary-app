94fd4cc4f91a91b744aceb91bdaf69a6d5d987e7
Paulo Coelho <9609090+prscoelho@users.noreply.github.com>
2021-03-04 19:33:39 +0000

    Sync localstorage with redux storage to preserve data

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,8 +2,18 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import rootReducer from './rootReducer'
 
+const serializedState = window.localStorage.getItem("state")
+const localState = serializedState ? JSON.parse(serializedState) : undefined
+
 const store = configureStore({
-    reducer: rootReducer
+    reducer: rootReducer,
+    preloadedState: localState
+})
+
+store.subscribe(() => {
+    const state = store.getState()
+    const serialized = JSON.stringify(state)
+    window.localStorage.setItem("state", serialized)
 })
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
